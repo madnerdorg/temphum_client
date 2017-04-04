71719dc8e296553e1aaf17ed9429f5e50c9f01c8
Rémi Sarrailh <remi@madnerd.org>
2017-04-04 14:21:20 +0200

    Add code
    I shouldn't add node_modules but i'm lazy.

---
+++ readme.md
@@ -0,0 +1,37 @@
+NodeJS Client Example for temperature / sensor and leds
+---------------------------------------------------------
+This example save data from DHT11 into a csv file and if there is leds display    
+a bar depending on temperature.
+
+This example shows that you can reuse your javascript code inside a client
+with little modifications, so you can prototype with html files and then transfer it   
+into a headless client.
+
+I'm using rwebsocket to manage reconnection, all the alternative failed to works (probably because
+i rush thought its) or use syntax that makes them incompatible.
+
+# Build
+Go to [temphum github](https://github.com/madnerdorg/temphum) to learn how to build the device.
+
+# Install
+You will need nodejs
+```
+npm i rwebsocket -S
+```
+
+# Usage
+Terminal
+```
+node temphum.js
+```
+
+/etc/rc.local
+```
+cd /dir;/usr/local/bin/node temphum.js
+```
+
+# Issues
+It seems that the client won't start, if no network is present.    
+If you want to use it networkless, you need to connect it first (using your phone for ex.)    
+I need to investigate this but this will not be easy to test this without network   
+I have a console cable so it should do the trick    
---
+++ temphum.js
@@ -0,0 +1,116 @@
+WebSocket = require('rwebsocket');
+var fs = require('fs');
+
+cold = 19;
+confort = 23;
+hot = 28;
+veryhot = 32;
+
+leds = new WebSocket("ws://localhost:42001");
+leds.onopen = ledsOK;
+current_humidity = null;
+current_temp = null; 
+first_data = true;
+data_timing = 1000;
+data_point = 0;
+
+sensor = new WebSocket("ws://localhost:42004");
+sensor.onmessage = readSensor;
+sensor.onopen = connectionOK;
+sensor.onclose = ledsFailed;
+sensor.onerror  = ledsFailed;
+
+leds.connect();
+sensor.connect();
+
+function ledsFailed(){
+  try{
+    leds.send("7;7;7;7;7");
+  }
+  catch (err){
+    console.log("Can't control leds right now");
+  }
+}
+
+function connectionOK(){
+  sensor.send("/info");
+  setInterval(function() {
+    save_data();
+  }, data_timing);
+}
+
+function ledsOK(){
+  leds.send("/info");
+  display_temp(current_temp,true);
+}
+
+function readSensor(event){
+
+  if(event.data !== "temphum:42004"){
+    sensor_value = event.data.split(";");
+    humidity = sensor_value[0];    
+    temperature = sensor_value[1];
+    
+    display_temp(temperature,false);
+    current_humidity = humidity;
+    current_temp = temperature;
+
+    if(first_data){
+      save_data();
+      first_data = false;
+    }
+  }
+
+}
+
+function save_data(){
+  if(current_temp !== null){
+    data_point = data_point + 1;
+    console.log(data_point + "," + current_temp + "," + current_humidity);
+    fs.appendFile("sensors.csv", data_point + "," + current_temp + "," + current_humidity + "\n", function(err){
+      if(err) {
+        return console.log(err);
+      }
+    });
+    console.log("Saving sensors.csv");
+  }
+}
+
+function display_temp(temp,reset){
+  var temp_icon;
+  try{
+    if((temp != current_temp) || (reset)){
+
+      if(temp < cold){
+
+       leds.send("4");
+     }
+
+     if(temp >= cold && temp < confort){
+
+      leds.send("4;4");
+
+    }
+
+    if(temp >= confort && temp < hot){
+
+     leds.send("5;5;5");
+
+   }
+
+   if(temp >= hot && temp < veryhot){
+
+    leds.send("6;6;6;6");
+
+  }
+
+  if(temp >= veryhot){
+
+    leds.send("2;2;2;2;2");
+
+  }
+}
+} catch (err){
+  //console.log("Failed to send to leds");
+}
+}
